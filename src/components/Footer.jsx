a71021a62a5de2383cf20082a5acf993d8f9c1cd
Breyner Ustariz <breynersmithustariz@mail.com>
2023-11-23 16:58:51 -0500

    TERMINANDO RESPONSIVE

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@
 
 export default function Footer() {
   return (
-    <div className="mt-10 bg-[#252F3D] text-white text-center py-5 text-xs px-5 flex-col">
+    <div className=" bg-[#252F3D] text-white text-center py-5 text-xs px-5  lg:flex lg:justify-between lg:px-16">
       <p className="flex  justify-center">
       Copyright (c) 2023 <span className="px-2">|</span>  Clarifionsupport@clarifion.com
       </p>
-      <p className="mt-4 flex gap-2 justify-center"><img src="img/icon-candado.svg" alt="icon candado" />Secure 256-bit SSL encryption.</p>
+      <p className="mt-4 lg:mt-0 flex gap-2 justify-center"><img src="img/icon-candado.svg" alt="icon candado" />Secure 256-bit SSL encryption.</p>
     </div>
   )
 }
